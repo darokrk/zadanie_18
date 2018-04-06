b4312435defa4e293534e066939c48a32e847232
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-06 13:55:31 +0200

    Added function to support new chat user

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+//function to support new chat user
+io.on('connection', function(socket) {
+    //the client listens to the chat entry message
+    socket.on('join', function(name) {
+        // we save the user who appeared in the application to the website holding the list of people in the chat
+        userService.addUser({
+            id: socket.id,
+            name
+        });
+        // the application emits an update event which updates information about the list of users to everyone listening to the 'update' event
+        io.emit('update', {
+            users: userService.getAllUsers()
+        });
+    });
+});
+
 server.listen(3000, function() {
     console.log('listening on *:3000');
 });
